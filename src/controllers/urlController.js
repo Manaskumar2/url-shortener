912497b00c7c9d436233f477728513637e529453
mahajansaurav02 <mahajansaurav02@gmail.com>
2022-10-03 17:01:44 +0530

    add caching

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,8 +2,29 @@
 const urlModel=require("../models/urlModel")
 const shortId=require('shortid')
 const validUrl=require('valid-url')
-// const { create } = require("../models/urlModel")
 const baseurl="http://localhost:3000"
+const redis=require('redis')
+const {promisify}=require('util')
+
+//---------------------------------------- Caching process------------------------------------------------------------------------------------
+
+const redisClient = redis.createClient(
+    15984,
+    "redis-15984.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("rNej1h645Ci9IMVnTaN4EfNEHubi7rvg", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+  });
+  
+  
+  const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+  const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
 
 //=================================== Create Url Api================================================================
 
@@ -28,12 +49,19 @@ let createUrl=async function(req,res){
             return res.status(400).send({status:false,message:"Invalid Baseurl"})
         }
         
+    let checkUnique= await GET_ASYNC(`${longUrl}`)
+    if(checkUnique){
+
+    return res.status(200).send({status:true,data:checkUnique})
+    }else{
+        
         let checkUrl=await urlModel.findOne({longUrl:longUrl}).select({_id:0,__v:0,createdAt:0,updatedAt:0})
         
-        if(checkUrl){
+        await SET_ASYNC(`${longUrl}`,JSON.stringify(checkUrl))
+
             return res.status(200).send({status:true,data:checkUrl})
-        }
-        
+
+    } 
         let generateUrl= shortId.generate(longUrl).toLowerCase()
                 
         let createShortUrl=baseurl+"/"+generateUrl
@@ -64,7 +92,9 @@ let createUrl=async function(req,res){
 
 //=================================== get Url Api================================================================
 
+
 let getUrl= async function (req,res){
+
     try {
         let urlCode=req.params.urlCode
 
@@ -77,22 +107,34 @@ let getUrl= async function (req,res){
         if (!shortId.isValid(urlCode)){
             return res.status(400).send({status:false,message:"Invalid urlCode"})
         }
-        //---------- uniqeness for Url code----------------
         
-        let checkUrlCode= await urlModel.findOne({urlCode:urlCode})
+    let checkCached= await GET_ASYNC(`${urlCode}`)
+
+//------------------ Hit ---------------------------------------
+
+if(checkCached){
+    return res.status(200).redirect(checkCached)
+}else{
+    
+    let checkUrlCode= await urlModel.findOne({urlCode:urlCode})
+    
+    if(!checkUrlCode){
+        return res.status(404).send({status:false,message:"No url found"})}
         
-        if(!checkUrlCode){
-            return res.status(404).send({status:false,message:"No url found"})
-        }
-        //---------- Redirecting for LongUrl ----------------
-        
+//------------------ Miss ---------------------------------------
+
+    await SET_ASYNC(`${urlCode}`,JSON.stringify(checkUrlCode.longUrl))
+    
         return res.status(302).redirect(checkUrlCode.longUrl)
-        
+                 
+}  
     } catch (error) {
     return res.status(500).send({status:false,message:error.message})
         
     }
 }
+
+//=================================================================================================
 
 module.exports={
     createUrl,getUrl
